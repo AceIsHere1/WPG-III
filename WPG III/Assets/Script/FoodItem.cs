b7d1c0bc76b0848e0705afd0963845e51bc870fe
Almer Abrilgi <aceealmer@gmail.com>
2025-08-30 10:08:01 +0700

    Update FoodItem.cs

--- WPG III/Assets/Script/FoodItem.cs
+++ WPG III/Assets/Script/FoodItem.cs
@@ -2,43 +2,46 @@ using UnityEngine;
 
 public class FoodItem : MonoBehaviour
 {
-    public KeyCode interactKey = KeyCode.E;
-    private bool isPlayerNearby = false;
-    private bool isHeld = false;
+    private bool isPickedUp = false;
     private Transform playerHand;
-
-    void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            isPlayerNearby = true;
-            playerHand = other.transform.Find("HandPoint");
-            Debug.Log("Dekat makanan - Tekan E untuk mengambil.");
-        }
-    }
-
-    void OnTriggerExit(Collider other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            isPlayerNearby = false;
-            playerHand = null;
-        }
-    }
 
     void Update()
     {
-        if (isPlayerNearby && !isHeld && Input.GetKeyDown(interactKey))
+        if (isPickedUp && playerHand != null)
         {
-            if (playerHand != null)
-            {
-                // tempel ke tangan
-                transform.SetParent(playerHand);
-                transform.localPosition = Vector3.zero;
-                transform.localRotation = Quaternion.identity;
+            // makanan akan terus nempel di tangan
+            transform.position = playerHand.position;
+            transform.rotation = playerHand.rotation;
+        }
+    }
 
-                isHeld = true;
-                Debug.Log("Makanan dipegang player!");
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (Input.GetKeyDown(KeyCode.E)) // tekan E untuk ambil
+            {
+                if (!isPickedUp)
+                {
+                    // cari objek tangan di player
+                    playerHand = other.transform.Find("Hand");
+
+                    if (playerHand != null)
+                    {
+                        isPickedUp = true;
+
+                        // supaya makanan ikut tangan
+                        transform.SetParent(playerHand);
+
+                        // atur posisi relatif ke tangan
+                        transform.localPosition = Vector3.zero;
+                        transform.localRotation = Quaternion.identity;
+                    }
+                    else
+                    {
+                        Debug.LogWarning("Player tidak punya objek bernama 'Hand'");
+                    }
+                }
             }
         }
     }
