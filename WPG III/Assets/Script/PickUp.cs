8a431e680661c0b14d696e54d60c922c23499b79
Almer Abrilgi <aceealmer@gmail.com>
2025-08-30 15:40:19 +0700

    Update PickUp.cs

--- WPG III/Assets/Script/PickUp.cs
+++ WPG III/Assets/Script/PickUp.cs
@@ -1,48 +1,35 @@
 ﻿using UnityEngine;
 
 public class Pickup : MonoBehaviour
-{
-    public float pickupRange = 3f;   // jarak bisa ambil
-    public LayerMask pickupLayer;    // layer objek yang bisa diambil
-    public Transform holdParent;     // tempat objek ditaruh (misal depan kamera)
 
-    private GameObject heldObject;
+{
+    public Transform pickupPoint;
+
+    void Start()
+    {
+        
+    }
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0)) // klik kiri
-        {
-            if (heldObject == null)
-            {
-                // coba ambil objek
-                TryPickup();
-            }
-            else
-            {
-                // kalau sudah pegang → lepas
-                Drop();
-            }
-        }
+        
     }
 
-    void TryPickup()
+
+    void OnMouseDown()
     {
-        Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
-        RaycastHit hit;
-
-        if (Physics.Raycast(ray, out hit, pickupRange, pickupLayer))
-        {
-            heldObject = hit.collider.gameObject;
-            heldObject.GetComponent<Rigidbody>().isKinematic = true;
-            heldObject.transform.position = holdParent.position;
-            heldObject.transform.parent = holdParent;
-        }
+        transform.parent = pickupPoint.transform;
     }
 
-    void Drop()
+
+    void OnMouseUp()
     {
-        heldObject.GetComponent<Rigidbody>().isKinematic = false;
-        heldObject.transform.parent = null;
-        heldObject = null;
+        transform.parent = null;
     }
+
+
+
+
+
 }
+
