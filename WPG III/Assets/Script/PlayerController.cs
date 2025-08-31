fb4a1dfc6572ddeceec072dcbada5c2c90e24def
Bagaskara Dwi Maryanto <bagaskaradwimaryanto@gmail.com>
2025-09-01 00:29:01 +0700

    add walking animation
    masih ada bug di arah kameranya. pergerakan karakter tidak bisa mengikuti arah kamera

--- WPG III/Assets/Script/PlayerController.cs
+++ WPG III/Assets/Script/PlayerController.cs
@@ -4,17 +4,24 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    //camera movement
     [SerializeField] Transform playerCamera = null;
     [SerializeField] float mouseSensitivity = 3.5f;
-    [SerializeField] float walkSpeed = 6.0f;
+
+    //character movement
+    [SerializeField] private int speed = 5;
+    [SerializeField] private Vector3 moveDirection;
+    [SerializeField] private CharacterController controller;
 
     [SerializeField] bool lockCursor = true;
 
     float cameraPitch = 0.0f;
-    CharacterController controller = null;
+
+    private Animator animator;
 
     void Start()
     {
+        animator = GetComponent<Animator>();
         controller = GetComponent<CharacterController>();
         if (lockCursor) 
         {
@@ -45,11 +52,62 @@ public class PlayerController : MonoBehaviour
 
     void UpdateMovement()
     {
-        Vector2 inputDir = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        inputDir.Normalize();
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
 
-        Vector3 velocity = (transform.forward * inputDir.y + transform.right * inputDir.x) * walkSpeed;
+        // Ambil arah kamera
+        //Vector3 camForward = playerCamera.transform.forward;
+        //Vector3 camRight = playerCamera.transform.right;
 
-        controller.Move(velocity * Time.deltaTime);
+        // Buat flat (hilangkan komponen Y biar tidak miring)
+        //camForward.y = 0f;
+        //camRight.y = 0f;
+
+        //camForward.Normalize();
+        //camRight.Normalize();
+
+        // Hitung arah gerakan berdasarkan kamera
+        //Vector3 moveDirection = (camForward * vertical + camRight * horizontal).normalized;
+
+        // Kalau ada input (bukan idle)
+        //if (moveDirection.magnitude > 0.1f)
+        //{
+            // Rotasi player ke arah gerakan
+            //transform.rotation = Quaternion.Slerp(
+                //transform.rotation,
+                //Quaternion.LookRotation(moveDirection),
+                //Time.deltaTime * 10f
+            //);
+        //}
+
+        // Gerakkan player
+        //controller.Move(moveDirection * speed * Time.deltaTime);
+
+        moveDirection = new Vector3(horizontal, 0, vertical);
+
+        moveDirection *= speed;
+
+        if ((moveDirection.x != 0) || (moveDirection.z != 0))
+        {
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(moveDirection), Time.deltaTime);
+        }
+
+        controller.Move(moveDirection * Time.deltaTime);
+
+        //Vector2 inputDir = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        //inputDir.Normalize();
+
+        //Vector3 velocity = (transform.forward * inputDir.y + transform.right * inputDir.x) * speed;
+
+        //.Move(velocity * Time.deltaTime);
+
+        if (moveDirection == Vector3.zero)
+        {
+            animator.SetFloat("Speed", 0);
+        }
+        else
+        {
+            animator.SetFloat("Speed", 0.5f);
+        }
     }
 }
