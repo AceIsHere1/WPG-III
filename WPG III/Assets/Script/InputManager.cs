5fdd001c8aef09f884fe6a41ed6b50306818928d
Bagaskara Dwi Maryanto <bagaskaradwimaryanto@gmail.com>
2025-09-03 01:22:42 +0700

    menambahkan animasi jalan maju, mundur, ke samping kanan dan kiri.
    camera belum

---
+++ WPG III/Assets/Script/InputManager.cs
@@ -0,0 +1,53 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Interactions;
+
+namespace UnityTutorial.Manager
+{
+
+    public class InputManager : MonoBehaviour
+    {
+        [SerializeField] private PlayerInput PlayerInput;
+
+        public Vector2 Move { get; private set; }
+        public Vector2 Look { get; private set; }
+
+        private InputActionMap _currentMap;
+        private InputAction _moveAction;
+        private InputAction _lookAction;
+
+        private void Awake()
+        {
+            _currentMap = PlayerInput.currentActionMap;
+            _moveAction = _currentMap.FindAction("Move");
+            _lookAction = _currentMap.FindAction("Look");
+
+            _moveAction.performed += onMove;
+            _lookAction.performed += onLook;
+
+            _moveAction.canceled += onMove;
+            _lookAction.canceled += onLook;
+        }
+
+        private void onMove(InputAction.CallbackContext context)
+        {
+            Move = context.ReadValue<Vector2>();
+        }
+
+        private void onLook(InputAction.CallbackContext context)
+        {
+            Look = context.ReadValue<Vector2>();
+        }
+
+        private void OnEnable()
+        {
+            _currentMap.Enable();
+        }
+
+        private void OnDisable()
+        {
+            _currentMap.Disable();
+        }
+    }
+}
