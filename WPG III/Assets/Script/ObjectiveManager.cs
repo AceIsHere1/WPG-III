6dbdf754265a900a637669def5914c865a99907e
Almer Abrilgi <aceealmer@gmail.com>
2025-10-13 17:15:30 +0700

    UI Objective

---
+++ WPG III/Assets/Script/ObjectiveManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using TMPro;
+using System.Collections;
+
+public class ObjectiveManager : MonoBehaviour
+{
+    public TextMeshProUGUI objectiveText;
+
+    void Start()
+    {
+        StartCoroutine(RunObjectives());
+    }
+
+    IEnumerator RunObjectives()
+    {
+        // Objective 1
+        objectiveText.text = " Objective: Layani pelanggan pertama!";
+        yield return new WaitForSeconds(10f); // tunggu 10 detik (bisa diubah)
+
+        // Objective 2
+        objectiveText.text = " Objective: Buang sesajen sebelum hantu muncul!";
+        yield return new WaitForSeconds(10f);
+
+        // Objective 3
+        objectiveText.text = " Objective: Layani pelanggan berikutnya!";
+        yield return new WaitForSeconds(10f);
+
+        // Objective 4 (Final)
+        objectiveText.text = " Semua objektif selesai! Klik kiri untuk mengambil sesuatu.";
+    }
+}
