ac85b9337eee27b3ccbeb41cce9e9b0eb07d1900
Almer Abrilgi <aceealmer@gmail.com>
2025-10-13 14:51:24 +0700

    how to play

--- WPG III/Assets/Script/HowToPlayMenu.cs
+++ WPG III/Assets/Script/HowToPlayMenu.cs
@@ -3,14 +3,28 @@ using UnityEngine;
 public class HowToPlayMenu : MonoBehaviour
 {
     public GameObject howToPlayPanel;
+    private bool isPanelActive = false;
+
+    void Update()
+    {
+        // Cek input ESC
+        if (Input.GetKeyDown(KeyCode.Escape) && isPanelActive)
+        {
+            CloseHowToPlay();
+        }
+    }
 
     public void ShowHowToPlay()
     {
         howToPlayPanel.SetActive(true);
+        isPanelActive = true;
+        Time.timeScale = 0f; // opsional, pause game kalau mau
     }
 
     public void CloseHowToPlay()
     {
         howToPlayPanel.SetActive(false);
+        isPanelActive = false;
+        Time.timeScale = 1f;
     }
 }
