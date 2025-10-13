7a565e3aa488983a1c36a764866b2832b1184df5
Almer Abrilgi <aceealmer@gmail.com>
2025-10-13 17:19:35 +0700

    Update ObjectiveManager.cs

--- WPG III/Assets/Script/ObjectiveManager.cs
+++ WPG III/Assets/Script/ObjectiveManager.cs
@@ -1,6 +1,5 @@
 ﻿using UnityEngine;
 using TMPro;
-using System.Collections;
 
 public class ObjectiveManager : MonoBehaviour
 {
@@ -8,24 +7,10 @@ public class ObjectiveManager : MonoBehaviour
 
     void Start()
     {
-        StartCoroutine(RunObjectives());
-    }
-
-    IEnumerator RunObjectives()
-    {
-        // Objective 1
-        objectiveText.text = " Objective: Layani pelanggan pertama!";
-        yield return new WaitForSeconds(10f); // tunggu 10 detik (bisa diubah)
-
-        // Objective 2
-        objectiveText.text = " Objective: Buang sesajen sebelum hantu muncul!";
-        yield return new WaitForSeconds(10f);
-
-        // Objective 3
-        objectiveText.text = " Objective: Layani pelanggan berikutnya!";
-        yield return new WaitForSeconds(10f);
-
-        // Objective 4 (Final)
-        objectiveText.text = " Semua objektif selesai! Klik kiri untuk mengambil sesuatu.";
+        objectiveText.text =
+            "  Objective:\n" +
+            "- Layani 2 pelanggan\n" +
+            "- Buang sesajen sebelum hantu menangkap mu!!!\n" +
+            "- Klik kiri untuk mengambil sesuatu";
     }
 }
